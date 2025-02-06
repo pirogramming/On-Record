81d91ccd8dc0395bef4224bfe3f22a26136ae357
l-wanderer01 <jhjjang0423@naver.com>
2025-02-07 02:01:11 +0900

    fix: Friend 테이블을 Pet 테이블로 테이블 명 수정 완료

--- diaries/admin.py
+++ diaries/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Diary)
-admin.site.register(Friend)
+admin.site.register(Pet)
 admin.site.register(Like)
 admin.site.register(Comment)
--- diaries/forms.py
+++ diaries/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 # from django.forms import ModelForm
-from .models import Diary, Friend, Personality, Plant
+from .models import Diary, Pet, Personality, Plant
 
 # 반려동물 등록 폼
-class FriendForm(forms.ModelForm):
+class PetForm(forms.ModelForm):
     personal = forms.ModelMultipleChoiceField(
         queryset = Personality.objects.all(), # 성격 목록을 가져옴
         widget = forms.CheckboxSelectMultiple, # 체크 박스로 표시
@@ -11,16 +11,16 @@ class FriendForm(forms.ModelForm):
     )
 
     class Meta:
-        model = Friend
+        model = Pet
         fields = ['name', 'kind', 'gender', 'age', 'personal', 'image', 'pet_fav', 'pet_hate', 'pet_sig']
 
 class PlantForm(forms.ModelForm):
 
     class Meta:
         model = Plant
-        fields = ['plant_kind', 'plant_name', 'plant_age', 'plant_image', 'plant_con', 'plant_sig', 'plant_adv']
+        fields = ['kind', 'name', 'age', 'image', 'plant_con', 'plant_sig', 'plant_adv']
         widgets = {
-            'plant_age': forms.NumberInput(attrs={'placeholder': '개월 수를 입력하세요'}),
+            'age': forms.NumberInput(attrs={'placeholder': '개월 수를 입력하세요'}),
             'plant_con': forms.TextInput(attrs={'placeholder': '요즘 이파리가 시들시들해요'}),
             'plant_sig': forms.TextInput(attrs={'placeholder': '꽃 향기가 좋아요'}),
             'plant_adv': forms.TextInput(attrs={'placeholder': '한결같이 우리집 베란다에 있는 모습'}),
@@ -33,4 +33,4 @@ class DiaryForm(forms.ModelForm):
 
     class Meta:
         model = Diary
-        fields = ['title', 'weather', 'content', 'friend','image', 'disclosure', 'date', 'mood']
+        fields = ['title', 'weather', 'content', 'pet','image', 'disclosure', 'date', 'mood']
