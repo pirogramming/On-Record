56158f83a8d2f44f5b1afd4ab67c5abe98b3ec3b
l-wanderer01 <jhjjang0423@naver.com>
2025-02-07 19:49:48 +0900

    add: 반려식물 form에도 이미지 미리보기 적용

--- diaries/urls.py
+++ diaries/urls.py
@@ -39,9 +39,6 @@ urlpatterns = [
     #11
     path('write_diaries' , write_diaries , name="write_diaries"),
 
-    #12
-    path('create_diaries' , create_diaries , name = 'create_diaries'),
-
     #13
     # path('detail_diaries_by_friend_date/<int:friend_id>/<int:selected_date>' , detail_diaries_by_friend_date , name = 'detail_diaries_by_friend_date')
 ]
