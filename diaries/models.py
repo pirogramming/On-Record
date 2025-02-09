3bd775575d8310b26653e0b7a2fcded6437afdba
HAN JONGSEO <pantene74@naver.com>
2025-02-09 17:42:20 +0900

    add: 로고 .svg -> .png 수정, friend-list.html 삭제, Diary model에서 plant 테이블 추가, friend_list.html 온기록 보기/남기기 로직 추가

--- diaries/models.py
+++ diaries/models.py
@@ -79,7 +79,8 @@ class Diary(models.Model):
     date = models.DateField(blank=True, null=True) # 기본 값을 현재 시간으로 설정
     mood = models.CharField(max_length=10, choices=MOOD_CHOICES, default='happy')
     weather = models.CharField(max_length=10, choices=WEATHER_CHOICES, default='sunny')
-    pet = models.ForeignKey(Pet , on_delete=models.CASCADE , null=True , blank=True)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, null=True, blank=True)
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE, null=True, blank=True)
     # 누구한테 쓴건지
     def __str__(self):
         return self.title
