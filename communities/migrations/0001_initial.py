399871e941193c1973560a25fea104f17d111733
l-wanderer01 <jhjjang0423@naver.com>
2025-02-10 19:38:47 +0900

    fix: diaries의 마이그레이션 충돌나던 문제 해결

--- communities/migrations/0001_initial.py
+++ communities/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('diaries', '0006_remove_like_diary_remove_like_diary_user_and_more'),
+        ('diaries', '0007_remove_like_diary_remove_like_diary_user_and_more'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
