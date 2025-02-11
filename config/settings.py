d470fd92a01081ecb42eb55a147b97dbbeac35c1
cestjeudi <juyoung1006.cho@hufs.ac.kr>
2025-02-11 13:57:34 +0900

    fix: 충돌 해결

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
+OPENAI_API_KEY = env("OPENAI_API_KEY", default="")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
