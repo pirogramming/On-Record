8751b067f438fa8931914ded983aa422df2afb6b
HAN JONGSEO <pantene74@kw.ac.kr>
2025-02-17 01:13:47 +0900

    fix: 소셜 로그인 로직 원상복구

--- users/adapters.py
+++ users/adapters.py
@@ -1,38 +1,10 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.utils import valid_email_or_none
 from django.contrib.auth import get_user_model
-from allauth.socialaccount.models import SocialAccount
-from django.shortcuts import redirect
-from allauth.exceptions import ImmediateHttpResponse
 
 User = get_user_model()
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def pre_social_login(self, request, sociallogin):
-        # 소셜 로그인 시 기존 일반 로그인 이메일이 있으면 연결
-        email = valid_email_or_none(sociallogin.account.extra_data.get("email"))
-        provider = sociallogin.account.provider
-
-        if not email:
-            return
-
-        try:
-            user = User.objects.get(email=email)
-
-            if not user.socialaccount_set.filter(provider=provider).exists():
-                sociallogin.user = user
-                sociallogin.save(request)
-
-        except User.DoesNotExist:
-            pass
-
-    def save_user(self, request, sociallogin, form=None):
-        # 기존 유저가 있으면 소셜 계정과 연결, 없으면 새 계정을 생성
-        if sociallogin.user and sociallogin.user.is_authenticated:
-            return sociallogin.user
-        return super().save_user(request, sociallogin, form)
-
-
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
 
